ff61cfcd0dd76964be89b06714629f264d4292f8
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2019-10-29 22:34:33 +0100

    Fixed sorting algorithm and added sorting to graphMapper

--- src/__tests__/util/arrays-test.js
+++ src/__tests__/util/arrays-test.js
@@ -17,6 +17,7 @@ describe('Array util', () => {
       { id: 1, name : 'top', ref : null},
       { id: 2, name : 'first', ref : 1},
     ]
+
     const expected = [
       { id: 1, name : 'top', ref : null},
       { id: 2, name : 'first', ref : 1},
@@ -25,6 +26,27 @@ describe('Array util', () => {
     ]
     expect(sortByChainedRefs(unsorted1, linkFn)).toEqual(expected)
     expect(sortByChainedRefs(unsorted2, linkFn)).toEqual(expected)
+  })
+
+  test('It should sort some real life business processes', () => {
+    const linkFn = o => o.ref
+    const businessProcesses = [
+      { id: '1_2_3', name: '1_2_3 Utlede variabler - Lønn og tilsvarende ytelser', ref: '1_2_2' },
+      { id: '1_3_2', name: '1_3_2 Aggregere - Kombiner til samlet persondatasett', ref: '1_3_1' },
+      { id: '1_2_1', name: '1_2_1 Utlede variabler - Konto', ref: '1_1_1' },
+      { id: '1_2_2', name: '1_2_2 Utlede variabler - Eiendom', ref: '1_2_1' },
+      { id: '1_1_1', name: '1_1_1 Preprossessering - Skill ut skatteobjekter', ref: undefined },
+      { id: '1_3_1', name: '1_3_1 Aggregere - Skatteobjekter til person nivå', ref: '1_2_3' }
+    ]
+    const expected = [
+      { id: '1_1_1', name: '1_1_1 Preprossessering - Skill ut skatteobjekter', ref: undefined },
+      { id: '1_2_1', name: '1_2_1 Utlede variabler - Konto', ref: '1_1_1' },
+      { id: '1_2_2', name: '1_2_2 Utlede variabler - Eiendom', ref: '1_2_1' },
+      { id: '1_2_3', name: '1_2_3 Utlede variabler - Lønn og tilsvarende ytelser', ref: '1_2_2' },
+      { id: '1_3_1', name: '1_3_1 Aggregere - Skatteobjekter til person nivå', ref: '1_2_3' },
+      { id: '1_3_2', name: '1_3_2 Aggregere - Kombiner til samlet persondatasett', ref: '1_3_1' }
+    ]
+    expect(sortByChainedRefs(businessProcesses, linkFn)).toEqual(expected)
   })
 
 })
--- src/util/arrays.js
+++ src/util/arrays.js
@@ -1,4 +1,3 @@
-
 /**
  * This function sorts an array of chained elements based on the given link function.
  * Each element in the array can have a "parent" reference to one of the other
@@ -8,34 +7,28 @@
  * @param linkFn a link function that returns the id of the previous (linked) element
  * @returns [] sorted array
  */
-function sortByChainedRefs(array, linkFn) {
+function sortByChainedRefs (array, linkFn) {
   if (!linkFn) {
     return array
   } else {
-    let sorted = []
-    array.forEach(obj => {
-      const value = linkFn(obj)
-      if (!value) {
-        const other = sorted.findIndex(o => linkFn(o) === null)
-        if (other !== -1) {
-          // Preserve same ordering as original array
-          sorted.splice(other + 1, 0, obj)
-        } else {
-          // Place object with no ref first
-          sorted.unshift(obj)
-        }
-      } else {
-        const refsTo = sorted.findIndex(o => o.id === value)
-        const refsFrom = sorted.findIndex(o => obj.id === linkFn(o))
-        if (refsTo !== -1) {
-          sorted.splice(refsTo + 1, 0, obj)
-        } else if (refsFrom !== -1) {
-          sorted.splice(refsFrom, 0, obj)
-        } else {
-          sorted.push(obj)
+    const sorted = [...array]
+    let swapped = false
+    // Based on the bubble sort algorithm
+    for (let i = 0; i < sorted.length - 1; i++) {
+      swapped = false
+      for (let j = 0; j < sorted.length - 1; j++) {
+        if (sorted[j].id !== linkFn(sorted[j + 1])) {
+          // swap arr[j] and arr[j+1]
+          let temp = sorted[j]
+          sorted[j] = sorted[j + 1]
+          sorted[j + 1] = temp
+          swapped = true
         }
       }
-    })
+      // If no two elements were swapped by inner loop, then break
+      if (!swapped)
+        break
+    }
     return sorted
   }
 }
